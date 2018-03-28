2956ca18709a6d1188bdf1b07af849b8db7c72c3
SymbolixAU <dcooley@symbolix.com.au>
2018-03-28 19:53:39 +1100

    re-downloaded

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -7,28 +7,24 @@
 #define RAPIDJSON_ASSERT(x) ((void)0)
 #endif // RAPIDJSON_ASSERT
 
+/*
+ * git clone https://github.com/tencent/rapidjson --branch v1.1.0 --depth 1
+ */
+
+
+#include "rapidjson/document.h"
+/*
 #include "rapidjson/error/en.h"
 #include "rapidjson/filereadstream.h"
 #include "rapidjson/schema.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/prettywriter.h"
 
-#include "rapidjson/document.h"
 #include "rapidjson/allocators.h"
 using namespace rapidjson;
-
+*/
 
 #include <Rcpp.h>
 using namespace Rcpp;
 
 
-// [[Rcpp::export]]
-void testRapidjson(const char* json) {
-
-  Document document;
-  document.Parse(json);
-
-  std::string s = document["hello"].GetString();
-  Rcpp::Rcout << s << std::endl;
-
-}
